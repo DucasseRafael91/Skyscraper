aef166177de02f04e676a470247a31d9ccd1ef78
Rafael Ducasse <rafael.ducasse@fms-ea.com>
2025-10-21 10:59:57 +0200

    Ajout de docstring

--- gratte_ciel.py
+++ gratte_ciel.py
@@ -3,6 +3,11 @@
 import random
 
 def calcul_increase(sequence):
+    """
+    Permet de générer les bordures de la grille
+    :param sequence: liste de valeurs de la grille (bordure gauche ou haute )
+    :return: valeur à placer dans la bordure gauche ou haute
+    """
     count = 1
     max_seen = sequence[0]
     for val in sequence[1:]:
@@ -12,9 +17,19 @@ def calcul_increase(sequence):
     return count
 
 def calcul_increase_reverse(sequence):
+    """
+    Permet de générer les bordures de la grille
+    :param sequence: liste de valeurs de la grille (bordure droite ou basse )
+    :return: valeur à placer dans la bordure droite ou basse dans la grille
+    """
     return calcul_increase(sequence[::-1])
 
 def print_matrice(grid):
+    """
+    Permet l'affichage de la grille.
+    :param grid: Grille à afficher
+    :return: False si erreur ou sinon al grille
+    """
     for row in grid:
         print(" ".join(str(cell).rjust(2) for cell in row))
 
@@ -42,15 +57,25 @@ def create_game_grid(size):
     fill(0, 0)
     return grid
 
-def add_borders(core):
-    size = len(core)
+def add_borders(game_grid):
+    """
+    Permet d'ajouter les bordures de la grille à la zone de jeu.
+    :param game_grid: Zone de jeu
+    :return: Retourne la brille avec les bordures ajoutées
+    """
+    size = len(game_grid)
     grid = [['X'] + [' ' for _ in range(size)] + ['X']]
-    for row in core:
+    for row in game_grid:
         grid.append([' '] + row + [' '])  # middle rows with side borders
     grid.append(['X'] + [' ' for _ in range(size)] + ['X'])  # bottom border
     return grid
 
 def fill_borders(grid):
+    """
+    Permet de remplacer les valeurs X dans les borudres par les vraies valeurs
+    :param grid: Grille de jeu avec les bordures
+    :return: None
+    """
     size = len(grid) - 2
     for i in range(1, size + 1):
         row_vals = grid[i][1:-1]
@@ -62,6 +87,12 @@ def fill_borders(grid):
         grid[-1][i] = calcul_increase_reverse(col_vals)
 
 def hide_game_zone(grid, symbol):
+    """
+    Permet de remplacer les valeurs de la zone de jeu par un symbole voulue (X par défaut )
+    :param grid: Grille de jeu avec les bordures
+    :param symbol: symbole qui va remplacer les valeurs de la zone de jeu
+    :return: None
+    """
     for i in range(1, len(grid) - 1):
         for j in range(1, len(grid) - 1):
             grid[i][j] = symbol
@@ -72,8 +103,8 @@ def main():
         if user_input.isdigit():
             size = int(user_input)
             if 4 <= size <= 50:
-                core_grid = create_game_grid(size)
-                full_grid = add_borders(core_grid)
+                game_grid = create_game_grid(size)
+                full_grid = add_borders(game_grid)
                 fill_borders(full_grid)
 
                 print("\n GRILLE SOLUTION :\n")
